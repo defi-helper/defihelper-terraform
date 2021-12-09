525e7e8296476713d5dd34659dc67edf001d1af2
Vyacheslav Golovnin <krelion.s@gmail.com>
2021-12-09 11:09:56 +0000

    Remove redis centrifugo

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -33,39 +33,6 @@ resource "yandex_mdb_redis_cluster" "redis_regional" {
   }
 }
 
-resource "yandex_mdb_redis_cluster" "redis_regional_centrifugo" {
-  name        = "${var.redis_name}-centrifugo"
-  environment = "PRESTABLE"
-  sharded     = true
-  network_id = var.vpc_id
-
-  config {
-    password = local.password
-    version  = "6.0"
-  }
-
-  resources {
-    resource_preset_id = "b2.nano"
-    disk_size          = 16
-  }
-
-  host {
-    zone      = var.location_subnets[0].zone
-    subnet_id = var.location_subnets[0].id
-    shard_name = "first"
-  }
-  host {
-    zone      = var.location_subnets[1].zone
-    subnet_id = var.location_subnets[1].id
-    shard_name = "second"
-  }
-  host {
-    zone      = var.location_subnets[2].zone
-    subnet_id = var.location_subnets[2].id
-    shard_name = "third"
-  }
-}
-
 resource "random_string" "redis-password" {
   length  = 16
   special = false
