98e5bd65385152f48344e8125096846613e836ac
Vladislav Komissarov <mastyf@gmail.com>
2022-08-05 13:44:13 +0300

    access key / secret key export

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -5,3 +5,12 @@ output "s3_loki_static_access_key" {
 output "s3_loki_static_secret_key" {
   value = yandex_iam_service_account_static_access_key.sa-loki-static-key.secret_key
 }
+
+output "s3_static_access_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+}
+
+output "s3_static_secret_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+}
+
