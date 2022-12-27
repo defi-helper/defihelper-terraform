bd6b13e71ee5d7c2da4903d805d321fba7d6c54b
Vladislav Komissarov <mastyf@gmail.com>
2022-12-27 11:00:59 +0300

    prometheus upgrade

--- modules/prometheus/main.tf
+++ modules/prometheus/main.tf
@@ -750,7 +750,7 @@ resource "helm_release" "kube-prometheus-stack" {
   repository  = "https://prometheus-community.github.io/helm-charts"
   chart       = "kube-prometheus-stack"
   namespace   = kubernetes_namespace.prometheus.metadata[0].name
-  version     = "40.2.0"
+  version     = "41.7.3"
   values      = [yamlencode(local.values)]
   atomic      = true
 #  depends_on  = [module.crds.req]
