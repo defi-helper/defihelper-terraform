eee50e1566fbbfae4cb5f6c0aaa04204d907b9d2
Vladislav Komissarov <mastyf@gmail.com>
2022-12-28 14:34:25 +0300

    vpc default gw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,7 +9,7 @@ resource "yandex_vpc_subnet" "cluster_subnets" {
   v4_cidr_blocks = [cidrsubnet(var.subnet, length(var.zones)+1, count.index)]
   zone = var.zones[count.index]
   network_id = yandex_vpc_network.cluster.id
-#  route_table_id  = yandex_vpc_route_table.vpc_route_table[count.index].id
+  route_table_id  = yandex_vpc_route_table.vpc_route_table_default_gw[count.index].id
 }
 
 resource "yandex_vpc_route_table" "vpc_route_table" {
@@ -22,4 +22,21 @@ resource "yandex_vpc_route_table" "vpc_route_table" {
     destination_prefix = "0.0.0.0/0"
     next_hop_address   = cidrhost(cidrsubnet(var.subnet, length(var.zones)+1, 0),127)
   }
+}
+
+resource "yandex_vpc_route_table" "vpc_route_table_default_gw" {
+  count = length(var.zones)
+  name = "${var.name}-out-via-default-gw-${count.index}"
+  folder_id = var.folder_id
+  network_id = yandex_vpc_network.cluster.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id = yandex_vpc_gateway.default.id
+  }
+}
+
+resource "yandex_vpc_gateway" "default" {
+  name = "default-nat-gw"
+  shared_egress_gateway {}
 }
