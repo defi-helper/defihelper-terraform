3ce008f2a166bd26213b84c15626ffd17810a9dc
Vladislav Komissarov <mastyf@gmail.com>
2023-11-26 10:22:42 +0100

    new postgres cluster

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -186,3 +186,57 @@ resource "yandex_mdb_postgresql_cluster" "postgresql_cluster" {
     }
   }
 }
+
+
+resource "yandex_mdb_postgresql_database" "open" {
+  cluster_id = yandex_mdb_postgresql_cluster.postgresql_open.id
+  name       = "open"
+  owner      = var.pg_open_user_name
+  lc_collate = "en_US.UTF-8"
+  lc_type    = "en_US.UTF-8"
+}
+
+resource "yandex_mdb_postgresql_user" "open" {
+  cluster_id = yandex_mdb_postgresql_cluster.postgresql_open.id
+  name       = var.pg_open_user_name
+  password   = var.pg_open_user_password
+  conn_limit = var.pg_open_user_conn_limit
+}
+
+resource "yandex_mdb_postgresql_cluster" "postgresql_open" {
+  name        = "${var.name}-cluster-open"
+  folder_id   = var.folder_id
+  environment = "PRODUCTION"
+  network_id  = var.vpc_id
+
+  config {
+    version = var.pg_version
+
+    pooler_config {
+      pool_discard = false
+      pooling_mode = "SESSION"
+    }
+
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = var.enable_replication ? "local-ssd" : "network-ssd"
+      disk_size          = var.pg_instance_disk_size
+    }
+  }
+
+  dynamic "host" {
+    for_each = var.enable_replication ? [for conf in var.location_subnets : {
+      zone      = conf.zone
+      subnet_id = conf.id
+    }] : [{
+      zone      = var.location_subnets[0].zone
+      subnet_id = var.location_subnets[0].id
+    }]
+
+    content {
+      zone             = host.value.zone
+      subnet_id        = host.value.subnet_id
+      assign_public_ip = var.pg_public_ip
+    }
+  }
+}
